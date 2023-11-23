8f542c75cabca08ac58be5a315bc6facb22535fe
Akshay Benny <akshay@mx-zenbook>
2023-11-23 23:53:38 +0000

    edit

--- ElectronicBook.java
+++ ElectronicBook.java
@@ -15,6 +15,25 @@ class ElectronicResource {
     return fileSize;
   }
 
+  public void getElectronicResourceDetails() {
+    System.out.printf("The name of the book is %s", title);
+    System.out.printf("%s is the author", author);
+    System.out.printf("and it belongs to the genre %s", genre);
+    
+    if (canBeDownloaded) {
+      System.out.println("This book can be downloaded");
+    } else {
+      System.out.println("This book cannot be downloaded";)
+    }
+
+    if (fileSize.isEmpty()) {
+      System.out.printf("File size: This parameter has not been entered yet.");
+    } else {
+      System.out.printf("File size: %s", fileSize);
+    }
+
+  }
+
   // Mutators
   public void setCanBeDownloaded(boolean isDownloadable) {
     this.canBeDownloaded = isDownloadable;
