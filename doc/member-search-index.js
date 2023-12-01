444a80579fd21f843bf20a33d18a090986198f58
Akshay Benny <akshay@mx-zenbook>
2023-12-01 02:59:12 +0000

    javadoc generated

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"LibraryAccess","l":"getDuration()"},{"p":"<Unnamed>","c":"LibraryAccess","l":"getExpiryDate()"},{"p":"<Unnamed>","c":"LibraryAccess","l":"isExpired()"},{"p":"<Unnamed>","c":"LibraryAccess","l":"LibraryAccess(int)","u":"%3Cinit%3E(int)"},{"p":"<Unnamed>","c":"LibraryAccess","l":"setDuration(int)"}];updateSearchResults();
